83cb23689b70bd5f593df5adec1b2b7372d277ed
Toretay <abdymvzhit@gmail.com>
2025-04-23 14:33:23 +0500

    created textFormFields with text controllers

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,12 +9,61 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _messageController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Home Page"), centerTitle: true),
       body: SafeArea(
-        child: Center(child: Form(key: _formKey, child: Column(children: []))),
+        child: Center(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(maxWidth: 400),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  TextFormField(
+                    controller: _nameController,
+                    decoration: InputDecoration(
+                      hintText: "Your name",
+                      border: OutlineInputBorder(),
+                    ),
+                    validator:
+                        (value) =>
+                            (value == null || value.isEmpty)
+                                ? "Enter your name"
+                                : null,
+                  ),
+                  SizedBox(height: 16.0),
+                  TextFormField(
+                    controller: _nameController,
+                    decoration: InputDecoration(
+                      hintText: "Your email",
+                      border: OutlineInputBorder(),
+                    ),
+                    validator:
+                        (value) =>
+                            (value == null || value.isEmpty)
+                                ? "Enter your email"
+                                : null,
+                  ),
+                  SizedBox(height: 16.0),
+                  TextFormField(
+                    controller: _nameController,
+                    decoration: InputDecoration(
+                      hintText: "Your message",
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
