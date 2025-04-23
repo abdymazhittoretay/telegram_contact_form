1c5d5d304dd8077e08def6af2ef96a7ae8b55bc3
Toretay <abdymvzhit@gmail.com>
2025-04-23 15:04:05 +0500

    added pattern for email validation

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -45,11 +45,7 @@ class _HomePageState extends State<HomePage> {
                       hintText: "Your email",
                       border: OutlineInputBorder(),
                     ),
-                    validator:
-                        (value) =>
-                            (value == null || value.isEmpty)
-                                ? "Enter your email"
-                                : null,
+                    validator: validateEmail,
                   ),
                   SizedBox(height: 16.0),
                   TextFormField(
@@ -58,6 +54,11 @@ class _HomePageState extends State<HomePage> {
                       hintText: "Your message",
                       border: OutlineInputBorder(),
                     ),
+                    validator:
+                        (value) =>
+                            (value == null || value.isEmpty)
+                                ? "Enter your message"
+                                : null,
                     maxLines: 4,
                   ),
                   SizedBox(height: 32.0),
@@ -85,6 +86,22 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
     );
+  }
+
+  String? validateEmail(String? value) {
+    const pattern =
+        r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'"
+        r'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-'
+        r'\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*'
+        r'[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4]'
+        r'[0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9]'
+        r'[0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\'
+        r'x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])';
+    final regex = RegExp(pattern);
+
+    return value!.isNotEmpty && !regex.hasMatch(value)
+        ? 'Enter a valid email address'
+        : null;
   }
 
   void confirmationDialog() {
